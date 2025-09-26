b5f02b65f678892ea70bb9f9b59364a39f1ea57a
QingFeng-awa <qingfeng_awa@163.com>
2025-09-27 03:07:33 +0800

    build: 更新 zbputils 依赖至 v1.7.10
    更新 go.mod 和 banner.go 中的 zbputils 版本至 v1.7.10，同步更新相关依赖项

--- kanban/banner/banner.go
+++ kanban/banner/banner.go
@@ -5,7 +5,7 @@ package banner
 // Version ...
 var Version = "v2.0.5 (a62c06a)"
 var OriginalVersion = "v1.9.9 (e6a4dfc)"
-var zbputilsVersion = "v1.7.5 (6697080)"
+var zbputilsVersion = "v1.7.10 (8b76b32)"
 var originalZbputilsVersion = "v1.7.2 (2741050)"
 
 // Copyright ...
