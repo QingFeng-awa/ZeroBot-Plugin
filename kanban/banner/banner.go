1752c20751fed6bf6145d7e68cee918a5d508a6d
QingFeng-awa <qingfeng_awa@163.com>
2025-09-26 18:16:34 +0800

    chore: 更新banner中的版本号至v2.0.5

--- kanban/banner/banner.go
+++ kanban/banner/banner.go
@@ -3,7 +3,7 @@
 package banner
 
 // Version ...
-var Version = "v2.0.4 (a7ca9ad)"
+var Version = "v2.0.5 (a62c06a)"
 var OriginalVersion = "v1.9.9 (e6a4dfc)"
 var zbputilsVersion = "v1.7.5 (6697080)"
 var originalZbputilsVersion = "v1.7.2 (2741050)"
