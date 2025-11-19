d5da1d1a32c15629e4f4f902e16c3ef6b32807ae
QingFeng-awa <qingfeng_awa@163.com>
2025-11-19 11:37:10 +0800

    fix(qqwife): 修复获取用户性别信息时强制刷新的问题

--- plugin/qqwife/function.go
+++ plugin/qqwife/function.go
@@ -399,7 +399,7 @@ func init() {
 
 // getUserPronouns 获取用户性别信息
 func getUserPronouns(ctx *zero.Ctx, userID int64) string {
-	strangerInfo := ctx.GetStrangerInfo(userID, true)
+	strangerInfo := ctx.GetStrangerInfo(userID, false)
 	sex := strangerInfo.Get("sex").String()
 	switch sex {
 	case "male":
