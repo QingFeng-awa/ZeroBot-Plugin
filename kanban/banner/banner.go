24b8bb122995764fa7f28333e5423b81818b344d
QingFeng-awa <qingfeng_awa@163.com>
2025-11-25 18:40:26 +0800

    chore: 更新banner中的版本号至v2.4.0-alpha.20

--- kanban/banner/banner.go
+++ kanban/banner/banner.go
@@ -3,7 +3,7 @@
 package banner
 
 // Version ...
-var Version = "v2.4.0-alpha.19 (94192da)"
+var Version = "v2.4.0-alpha.20 (20e65fd)"
 var OriginalVersion = "v1.9.9 (e6a4dfc)"
 var zbputilsVersion = "v1.7.10 (8b76b32)"
 var originalZbputilsVersion = "v1.7.2 (2741050)"
