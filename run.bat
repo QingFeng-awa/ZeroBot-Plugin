6b9e9741f91c70f610cad12705caa1ad36998b9c
QingFeng-awa <qingfeng_awa@163.com>
2026-04-08 15:46:36 +0800

    chore: 更新GOPROXY为腾讯云镜像源

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 go version
-go env -w GOPROXY=https://goproxy.cn,direct
+go env -w GOPROXY=https://mirrors.tencentyun.com/go,direct
 go env -w GO111MODULE=auto
 go mod tidy
 ::go build -ldflags="-s -w" -o ZeroBot-Plugin.exe
